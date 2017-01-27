b85fee43a53a0a5021bd9c16d41f8658c4c7c4cc
owalekarnikita <nikitao8984@gmail.com>
2017-01-27 11:34:05 +0530

    contactus page with updation

---
+++ assets/js/scroll.js
@@ -0,0 +1,15 @@
+/*$(document).ready(function(){
+	$('window').mousein(.css('background-color','grey'));
+});*/
+
+/*$("#button").click(function(){
+    $("div").animate({left: '250px'});
+});
+/*$("button").click(function(){
+    $("div").animate({
+        left: '250px',
+        opacity: '0.5',
+        height: '150px',
+        width: '150px'
+    });
+});*/
